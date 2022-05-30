96cc3200ac06c44456f8c3479b9b3f69ad833408
bryanfortes <43735051+bryanfortes@users.noreply.github.com>
2022-05-30 17:21:57 -0400

    Integrated Semantic UI and added a searchbar

---
+++ src/main/frontend/src/components/App.js
@@ -0,0 +1,12 @@
+// import axios from "axios";
+import SearchBar from "./SearchBar";
+
+const App = () => {
+  return (
+    <div className={'ui container'} style={{marginTop: '10px'}}>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main/frontend/src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+   state = {term: ''};
+
+  render() {
+    return (
+      <div className={'ui segment'}>
+        <form className={'ui form'}>
+          <div className={'field'}>
+            <label>Card Search</label>
+            <input type={'text'}
+                   value={this.state.term}
+                   onChange={(event) => this.setState({term: event.target.value})}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
