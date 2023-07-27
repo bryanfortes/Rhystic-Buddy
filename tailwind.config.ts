474c19ce8cc206036dc02f1dcdeead5d1303e2be
Bryan Fortes <bryanfortes1@gmail.com>
2023-07-27 18:39:47 -0400

    Added Clerk to the project and landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,28 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "#000000",
+        background: "#ffffff",
+        primary: "#8fb3ff",
+        secondary: "#ebf1ff",
+        accent: "#d41d6d",
+      },
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          text: "#000000",
+          background: "#ffffff",
+          primary: "#8fb3ff",
+          secondary: "#ebf1ff",
+          accent: "#d41d6d",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 } satisfies Config;
